65afcab349eddf3f9dafae498d8d16a4905eb2bc
Rogalowski <rogalowski@gmail.com>
2021-06-19 14:45:03 +0200

    Add model Reservation Booking

--- booking_rooms/models.py
+++ booking_rooms/models.py
@@ -6,3 +6,12 @@ class Room(models.Model):
     name = models.TextField(max_length=255, unique=True)
     capacity = models.IntegerField()
     projector = models.BooleanField()
+
+class RoomBooking(models.Model):
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    date = models.DateField()
+    comment = models.TextField(null=True)
+
+
+class Meta:
+   unique_together = ('room_id', 'date',)
